1fac53849f80011062e19ae64d9d45a9a67abc35
samtraylor <68202899+samtraylor@users.noreply.github.com>
2020-07-14 18:18:30 -0500

    Updating to allow parsing of sequential games

--- RequestLogger.py
+++ RequestLogger.py
@@ -22,25 +22,25 @@ class S(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+        logging.info("Path: %s\nHeaders:\n%s\n\nBody:\n%s\n",
                 str(self.path), str(self.headers), post_data.decode('utf-8'))
 
         self._set_response()
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+        RequestParser.main('example.txt')
+        open("example.txt", "w").close()
 
 def run(server_class=HTTPServer, handler_class=S, port=8080):
     logging.basicConfig(filename='example.txt',level=logging.DEBUG) #File path argument must obviously be adjusted
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    logging.info('Starting httpd...\n')
+    #logging.info('Starting httpd...\n')
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
     logging.info('Stopping httpd...\n')
-    RequestParser.main('example.txt')
-    open("example.txt", "w").close()
     
 
 if __name__ == '__main__':
